23b5904d6074397fddd1d2f3b5712084b5e4eef1
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-10-13 17:33:15 -0400

    body parsing lol!

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var cors = require('cors');
 var multer = require('multer');
+var bodyParser = require('body-parser')
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -15,7 +19,7 @@ app.get('/', function (req, res) {
 
 app.post('/api/fileanalyse', function (req, res)
 {
-  res.json({funny: "funny"});
+  res.json({funny: "funny", body: req.body});
 });
 
 const port = process.env.PORT || 3000;
